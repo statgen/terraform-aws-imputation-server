56fa9f3722b2a6bbad9711452c32310b283c9b5c
Jacob Pleiness <pleiness@umich.edu>
2020-02-14 15:05:25 -0500

    Add ssm service policy to instance profile

--- modules/imputation-iam/main.tf
+++ modules/imputation-iam/main.tf
@@ -70,6 +70,11 @@ resource "aws_iam_role_policy_attachment" "cloudwatch" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.ec2.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
 resource "aws_iam_instance_profile" "ec2" {
   name = aws_iam_role.ec2.name
   role = aws_iam_role.ec2.name
